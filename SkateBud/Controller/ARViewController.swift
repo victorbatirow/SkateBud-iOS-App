bc3b1bbe11368b8c1f6bd2679ac5b574f6a17de0
victorbatirow <batirow@gmail.com>
2023-01-02 17:09:23 -0500

    AR interface and items menu created

---
+++ SkateBud/Controller/ARViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ARViewController.swift
+//  SkateBud
+//
+//  Created by Victor on 2023-01-02.
+//
+
+import UIKit
+import ARKit
+
+class ARViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Show AR debug elements in the scene
+        self.sceneView.debugOptions = [ ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        self.registerGestureRecognizers()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // hide navigation bar andtab bar when mapview opens
+//        self.navigationController?.navigationBar.isHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    func registerGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        if !hitTest.isEmpty {
+            self.addItem(hitTestResult: hitTest.first!)
+        }
+    }
+    
+    func addItem(hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem {
+            let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+            let node = scene?.rootNode.childNode(withName: selectedItem, recursively: false)
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node?.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ARCollectionViewItemCell", for: indexPath) as! ItemCollectionViewCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.green
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.lightGray
+    }
+}
