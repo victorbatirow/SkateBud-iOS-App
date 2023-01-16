283cc1c1366d4e03f89bba04f6a5d019ca5ebb3a
victorbatirow <batirow@gmail.com>
2023-01-15 19:26:42 -0500

    delete cone model

--- SkateBud/Controller/ARViewController.swift
+++ SkateBud/Controller/ARViewController.swift
@@ -14,7 +14,7 @@ class ARViewController: UIViewController, UICollectionViewDataSource, UICollecti
     @IBOutlet weak var planeDetectedLbl: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     
-    let itemsArray: [String] = ["cup", "vase", "boxing", "table", "skateboard", "cone", "cone1", "car1", "bench"]
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table", "skateboard", "cone1", "car1", "bench"]
     let configuration = ARWorldTrackingConfiguration()
     var selectedItem: String?
     
